aa7dc093932937317971d720c5bb11b026136313
Swati Krishnamurthy <swatikrishnamurthy24@gmail.com>
2026-08-27 09:24:24 +0000

    Sync LeetCode submission Runtime - 1 ms (22.81%), Memory - 48.9 MB (6.32%)

---
+++ 0344-reverse-string/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s, 0, s.length - 1);
+    }
+
+    void reverse(char[] s, int left, int right) {
+
+        if (left >= right)
+            return;
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        reverse(s, left + 1, right - 1);
+    }
+}
